4c5b0df3dbab818eddbdf0294bc3436d640aff1a
perturber <shubhamkejriwal@u.nus.edu>
2026-03-18 10:10:28 +0800

    update run_job helper

--- run_job.py
+++ run_job.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
                         help="Path to the sampling configuration JSON file.\n"
                              "Allowed keywords:\n"
                              "  - Param Buckets: evolving_params, static_params, fixed_evolving\n"
-                             "  - Sampler Setup: nwalkers, ntemps, nsteps, burn, thin_by\n"
-                             "  - Logistics: check_interval, check_converge, moves\n"
+                             "  - Sampler Setup: jitter, nwalkers, ntemps, nsteps, burn, thin_by\n"
+                             "  - Logistics: check_interval, check_converge\n"
+                             "  - Moves: moves, adapt_burn_steps, burn_in_mode_factor\n"
                              "  - File I/O: filename (or new_filename), old_filename")
     
     parser.add_argument('--out', type=str, default='SPLIT_Outputs',
